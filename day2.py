8848e85ae64adc3824b14c6afe411a1530631034
Sneha Reddy <snehareddy0151@gmail.com>
2025-01-08 22:15:31 +0530

    ggg

---
+++ day2.py
@@ -0,0 +1,19 @@
+my_list = [1,2,3,4,5]
+print("List:", my_list)
+print("Element at index 1:",my_list[1])
+print("Element at index 2:",my_list[4])
+
+my_tuple = ["a","b","c","d"]
+print("List:", my_tuple)
+print("Element at index 1:",my_tuple[1])
+print("Element at index 2:",my_tuple[4])
+
+my_dict = {
+    "name":"sita",
+    "age":15,
+    "gender":"Female"
+    "State":"Telangana"
+}
+print("Dictionary:", my_dict)
+print("Value of key name:",my_dict[name])
+print("Value of key age:",my_dict[age])
