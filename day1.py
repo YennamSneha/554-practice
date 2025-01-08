5100c07fa163931dd093d1fa00ed9aa6e7b70017
Sneha Reddy <snehareddy0151@gmail.com>
2025-01-08 21:56:09 +0530

    yy

---
+++ day1.py
@@ -0,0 +1,12 @@
+integer_var = 3
+float_var = 49.0
+string_var = "Hello"
+boolean_var = False
+
+print("Integer Variable:",integer_var,",Type:",type(integer_var) )
+
+print("Float Variable:",float_var,",Type:",type(float_var) )
+
+print("String Variable:",string_var,",Type:",type(string_var) )
+
+print("Boolean Variable:",boolean_var,",Type:",type(boolean_var) )
